6b0b481d305df4648ace1f0fb253877afde31d83
tjtarunjoshi <tjtarunjoshi10@gmail.com>
2021-11-05 13:03:22 +0530

    update teacher  file

--- learnvirtulum/src/components/teacher/teachersClass.js
+++ learnvirtulum/src/components/teacher/teachersClass.js
@@ -10,3 +10,15 @@ export default function TeachersClass() {
     </div>
   );
 }
+import React from "react";
+import NavigationBar from "./navigationbar";
+import Teacher from "./teacher/teacher";
+
+export default function TeachersClass() {
+  return (
+    <div>
+      <NavigationBar />
+      <Teacher />
+    </div>
+  );
+}
