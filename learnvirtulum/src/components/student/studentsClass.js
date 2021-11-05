002d39778b9b7d6104cd9c2a7299369b4769b6d4
tjtarunjoshi <tjtarunjoshi10@gmail.com>
2021-11-05 11:29:30 +0530

    update student file

---
+++ learnvirtulum/src/components/student/studentsClass.js
@@ -0,0 +1,12 @@
+import React from "react";
+import NavigationBar from "../navigationbar";
+import Student from "./student";
+
+export default function StudentsPage() {
+  return (
+    <div>
+      <NavigationBar />
+      <Student />
+    </div>
+  );
+}
