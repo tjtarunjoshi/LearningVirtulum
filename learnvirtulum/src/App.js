7fbe158b0286e5328bd97081be9de16f2dd7dcf0
tjtarunjoshi <tjtarunjoshi10@gmail.com>
2021-11-05 11:23:02 +0530

    update app.js file

--- learnvirtulum/src/App.js
+++ learnvirtulum/src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import "./components/styles.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "semantic-ui-css/semantic.min.css";
+import Login from "./components/Auth/Login";
+import Register from "./components/Auth/Register";
+import HeaderTop from "./components/header";
+import TeachersClass from "./components/teacher/teachersClass";
+import Footer from "./components/footer";
+import Profile from "./components/profilepage";
+import StudentsPage from "./components/student/studentsClass";
+import StudentLogin from "./components/Auth/StudentLogin";
+import StudentRegister from "./components/Auth/StudentRegister";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <HeaderTop />
+        <Switch>
+          <Route exact path="/app" component={App} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/register" component={Register} />
+          <Route exact path="/slogin" component={StudentLogin} />
+          <Route exact path="/sregister" component={StudentRegister} />
+          <Route exact path="/" component={TeachersClass} />
+          <Route exact path="/profile" component={Profile} />
+          <Route exact path="/student" component={StudentsPage} />
+        </Switch>
+        <Footer />
+      </Router>
     </div>
   );
 }
-
-export default App;
