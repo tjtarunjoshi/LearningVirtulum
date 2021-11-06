f09ccda5d42d5985739f0cdd5367825a8d9ba0fd
tjtarunjoshi <tjtarunjoshi10@gmail.com>
2021-11-06 23:02:58 +0530

    teacher update  and add nav file

---
+++ learnvirtulum/src/components/teacher/addQuiz/addQuiz.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Grid} from 'semantic-ui-react'
+import Panel1 from "../panel1";
+import TQuiz1 from './tQuiz1';
+import TQuiz2 from './tQuiz2';
+import NavigationBar from '../navigationbar';
+
+export default function AddQuiz() {
+  return (
+    <div className='teacher'>
+<NavigationBar/>      
+<Grid columns={3}>
+ <Grid.Row>
+   <Grid.Column width={4}>   
+       <Panel1/>    
+   </Grid.Column>
+   <Grid.Column width={6}>
+    <TQuiz1/>
+   </Grid.Column>
+   <Grid.Column>
+     <TQuiz2/>
+   </Grid.Column>
+ </Grid.Row>
+</Grid>
+ </div>
+  )
+}
