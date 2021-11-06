edda3d31d9e6e67da5709ee93de959feae08f34f
tjtarunjoshi <tjtarunjoshi10@gmail.com>
2021-11-06 23:17:19 +0530

    teacher update  and add nav file

---
+++ learnvirtulum/src/components/student/dowAssignment/sAssignment2.js
@@ -0,0 +1,38 @@
+import React from  'react';
+import { Grid, Form, Segment, Button, Header, Message, Icon } from 'semantic-ui-react';
+
+export default function TAssignment2() {
+  return (
+    <div>
+             <Grid centered verticalAlign='middle' className='panel3'>
+    <Grid.Column style={{ maxWidth : 450 }}>
+    <Icon name='paper plane' color='green' />
+      <Header as='h3' icon color='black'>
+        Add Assignment
+      </Header>
+      <Form size='large' textAlign='left'>
+        <Segment>    
+
+      
+          <Form.Field>
+            <label>Class Name:</label>
+            <input name='class' placeholder='Type your class name' onChange={(event) => setEmail(event.target.value)} type='text' />
+          </Form.Field>
+
+          <Form.Field>
+            <label>Subject</label>
+            <input name='subject' placeholder='Type your subject' onChange={(event) => setPassword(event.target.value)} type='text' />
+          </Form.Field>
+          <Form.Field>
+            <label>Google Drive Link</label>
+            <input name='topic' placeholder='Copy & Paste Google drive link' onChange={(event) => setPassword(event.target.value)} type='text' />
+          </Form.Field>
+
+          <Button color='Gray' fluid size='large'>Create Assignment </Button>  
+        </Segment>
+      </Form>
+    </Grid.Column>
+  </Grid>
+    </div>
+  )
+}
---
+++ learnvirtulum/src/components/student/dowQuiz/dowQuiz.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { Grid, GridColumn, Image } from "semantic-ui-react";
+import SPanel1 from "../spanel1";
+import SQuiz1 from "./sQuiz1";
+import SNavigationBar from "../snavigationbar";
+
+export default function DowQuiz() {
+  return (
+    <div className="teacher">
+      <SNavigationBar />
+      <Grid columns={3}>
+        <Grid.Row>
+          <Grid.Column width={4}>
+            <SPanel1 />
+          </Grid.Column>
+          <Grid.Column width={6}>
+            <SQuiz1 />
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
+    </div>
+  );
+}
