9ba21fb03d01f6459180014dd3f7dd5c77219a85
tjtarunjoshi <tjtarunjoshi10@gmail.com>
2021-11-06 22:56:41 +0530

    teacher update  and add nav file

---
+++ learnvirtulum/src/components/teacher/navigationbar.js
@@ -0,0 +1,59 @@
+import React, { useState } from "react";
+import { Input, Menu } from "semantic-ui-react";
+import { Link } from "react-router-dom";
+
+export default function NavigationBar() {
+  const [activeItem, setActiveItem] = useState("");
+
+  const handleItemClick = (e, { name }) => {
+    setActiveItem(name);
+  };
+
+  return (
+    <Menu pointing>
+      <Menu.Item
+        as={Link}
+        to="/tHome"
+        name="home"
+        active={activeItem === "home"}
+        onClick={handleItemClick}
+      />
+      <Menu.Item
+        as={Link}
+        to="/profile"
+        name="profile"
+        active={activeItem === "profile"}
+        onClick={handleItemClick}
+      />
+
+      <Menu.Item
+        as={Link}
+        to="/"
+        name="teacher"
+        active={activeItem === "teacher"}
+        onClick={handleItemClick}
+      />
+
+      <Menu.Item
+        as={Link}
+        to="/student"
+        name="student"
+        active={activeItem === "student"}
+        onClick={handleItemClick}
+      />
+
+      <Menu.Menu position="right">
+        <Menu.Item>
+          <Input icon="search" placeholder="Search..." />
+        </Menu.Item>
+        <Menu.Item
+          as={Link}
+          to="/login"
+          name="logout"
+          active={activeItem === "logout"}
+          onClick={handleItemClick}
+        />
+      </Menu.Menu>
+    </Menu>
+  );
+}
