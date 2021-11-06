362f95baa8a7cf77e0ceac60623147f0c51caf1c
tjtarunjoshi <tjtarunjoshi10@gmail.com>
2021-11-06 22:57:03 +0530

    teacher update  and add nav file

---
+++ learnvirtulum/src/components/teacher/addAssignment/addAssignment.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Grid} from 'semantic-ui-react'
+import Panel1 from "../panel1";
+import TAssignment1 from './tAssignment1';
+import TAssignment2 from './tAssignment2';
+import NavigationBar from '../navigationbar';
+
+export default function AddAssignment() {
+  return (
+    <div className='teacher'>
+<NavigationBar/>      
+<Grid columns={3}>
+ <Grid.Row>
+   <Grid.Column width={4}>   
+       <Panel1/>    
+   </Grid.Column>
+   <Grid.Column width={6}>
+    <TAssignment1/>
+   </Grid.Column>
+   <Grid.Column>
+     <TAssignment2/>
+   </Grid.Column>
+ </Grid.Row>
+</Grid>
+ </div>
+  )
+}
