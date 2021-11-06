e435f8e3055999553e9c14bdd245ba5c1ff264a1
tjtarunjoshi <tjtarunjoshi10@gmail.com>
2021-11-06 23:03:43 +0530

    teacher update  and add nav file

---
+++ learnvirtulum/src/components/teacher/addQuiz/tQuiz2.js
@@ -0,0 +1,38 @@
+import React from  'react';
+import { Grid, Form, Segment, Button, Header, Message, Icon } from 'semantic-ui-react';
+
+export default function TQuiz2() {
+  return (
+    <div>
+             <Grid centered verticalAlign='middle' className='panel3'>
+    <Grid.Column style={{ maxWidth : 450 }}>
+    <Icon name='paper plane' color='green' />
+      <Header as='h3' icon color='black'>
+        Add Quiz
+      </Header>
+      <Form size='large' textAlign='left'>
+        <Segment>    
+
+      
+          <Form.Field>
+            <label>Class Name:</label>
+            <input name='class' placeholder='Type your class name' onChange={(event) => setEmail(event.target.value)} type='text' />
+          </Form.Field>
+
+          <Form.Field>
+            <label>Subject</label>
+            <input name='subject' placeholder='Type your subject' onChange={(event) => setPassword(event.target.value)} type='text' />
+          </Form.Field>
+          <Form.Field>
+            <label>Google Form Link</label>
+            <input name='topic' placeholder='Copy & Paste Google Form link' onChange={(event) => setPassword(event.target.value)} type='text' />
+          </Form.Field>
+
+          <Button color='Gray' fluid size='large'>Create Quiz </Button>  
+        </Segment>
+      </Form>
+    </Grid.Column>
+  </Grid>
+    </div>
+  )
+}
