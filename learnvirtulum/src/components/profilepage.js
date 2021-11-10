f5bdf96a893aa8019b7fe8ae1f0bb7671a1d3240
Shreyas Naik <83569207+dashingshree6@users.noreply.github.com>
2021-11-10 23:51:34 +0530

    Delete profilepage.js

--- learnvirtulum/src/components/profilepage.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import { Grid, Image, Label, Segment,Table} from 'semantic-ui-react'
-import NavigationBar from './navigationbar';
-
-export default function Profile() {
-
-}
